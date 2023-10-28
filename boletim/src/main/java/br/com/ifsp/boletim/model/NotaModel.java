c9bc82d7e0ab8c604b0ba8b20b4d21889222036c
Aluno <KnuppLucas>
2023-10-27 21:40:54 -0300

    post and get

--- boletim/src/main/java/br/com/ifsp/boletim/model/NotaModel.java
+++ boletim/src/main/java/br/com/ifsp/boletim/model/NotaModel.java
@@ -1,10 +1,10 @@
 package br.com.ifsp.boletim.model;
 
-import jakarta.persistence.Entity;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
